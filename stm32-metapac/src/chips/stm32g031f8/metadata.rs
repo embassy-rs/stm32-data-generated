1f213612724dcd37f5caaefc19bb1a70560196e5
Embassy CI <ci@embassy.dev>
2024-04-17 15:14:39 +0000

    Generated from stm32-data 8ee7107b6d708f92d6226b14f598ee62929f5d67

--- stm32-metapac/src/chips/stm32g031f8/metadata.rs
+++ stm32-metapac/src/chips/stm32g031f8/metadata.rs
@@ -9,7 +9,7 @@ include!("../metadata_0269.rs");
         name: "BANK_1",
         kind: MemoryRegionKind::Flash,
         address: 0x8000000,
-        size: 32768,
+        size: 65536,
         settings: Some(
             FlashSettings {
                 erase_size: 2048,

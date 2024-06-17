e30c7694016fa9a326f153fff84dedb0c02b8676
Embassy CI <ci@embassy.dev>
2024-06-17 15:45:59 +0000

    Generated from stm32-data a8ab0a3421ed0ca4b282f54028a0a2decacd8631

--- stm32-metapac/src/chips/metadata_0207.rs
+++ stm32-metapac/src/chips/metadata_0207.rs
@@ -3083,7 +3083,14 @@
     Peripheral {
         name: "LTDC",
         address: 0x40016800,
-        registers: None,
+        registers: Some(
+            PeripheralRegisters {
+                kind: "ltdc",
+                version: "v1",
+                block: "LTDC",
+                ir: &ltdc::REGISTERS,
+            },
+        ),
         rcc: Some(
             PeripheralRcc {
                 bus_clock: "PCLK2",
@@ -8568,6 +8575,7 @@
 #[path="../registers/hash_v2.rs"] pub mod hash;
 #[path="../registers/i2c_v1.rs"] pub mod i2c;
 #[path="../registers/iwdg_v1.rs"] pub mod iwdg;
+#[path="../registers/ltdc_v1.rs"] pub mod ltdc;
 #[path="../registers/otg_v1.rs"] pub mod otg;
 #[path="../registers/pwr_f4.rs"] pub mod pwr;
 #[path="../registers/rcc_f4.rs"] pub mod rcc;

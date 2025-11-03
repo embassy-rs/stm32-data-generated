2399b94fb4c129a55248e3c6a6e098f4283fe667
Embassy CI <ci@embassy.dev>
2025-11-03 19:50:45 +0000

    Generated from stm32-data 63f566769a6cd0a47989ca1889d39efbf1f8d995

--- stm32-metapac/src/chips/stm32wle5j8/metadata.rs
+++ stm32-metapac/src/chips/stm32wle5j8/metadata.rs
@@ -1,4 +1,4 @@
-include!("../metadata_0872.rs");
+include!("../metadata_0874.rs");
 use crate::metadata::PeripheralRccKernelClock::{Clock, Mux};
 pub static METADATA: Metadata = Metadata {
     name: "STM32WLE5J8",

06a2362583a944bf9037706d1a7f47a62a6fdf21
Embassy CI <ci@embassy.dev>
2025-11-25 23:26:15 +0000

    Generated from stm32-data 3820de3d1180cbca3c4dae59b622d23f9a92fbee

--- stm32-metapac/src/chips/stm32wb05tz/metadata.rs
+++ stm32-metapac/src/chips/stm32wb05tz/metadata.rs
@@ -6,9 +6,20 @@ pub static METADATA: Metadata = Metadata {
     line: "STM32WBx5",
     memory: &[&[
         MemoryRegion {
+            name: "OTP",
+            kind: MemoryRegionKind::Flash,
+            address: 0x10001800,
+            size: 1024,
+            settings: Some(FlashSettings {
+                erase_size: 0,
+                write_size: 32,
+                erase_value: 255,
+            }),
+        },
+        MemoryRegion {
             name: "BANK_1",
             kind: MemoryRegionKind::Flash,
-            address: 0x1004000,
+            address: 0x10040000,
             size: 196608,
             settings: Some(FlashSettings {
                 erase_size: 2048,
@@ -19,20 +30,9 @@ pub static METADATA: Metadata = Metadata {
         MemoryRegion {
             name: "SRAM0",
             kind: MemoryRegionKind::Ram,
-            address: 0x2000000,
-            size: 12288,
+            address: 0x20000018,
+            size: 12264,
             settings: None,
-        },
-        MemoryRegion {
-            name: "OTP",
-            kind: MemoryRegionKind::Flash,
-            address: 0x10001800,
-            size: 1024,
-            settings: Some(FlashSettings {
-                erase_size: 0,
-                write_size: 32,
-                erase_value: 255,
-            }),
         },
         MemoryRegion {
             name: "SRAM1",
